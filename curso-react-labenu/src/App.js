d4c1418f9fd371f5edcbba79e28590b02d3d9099
Rafael Martins Machado <rafaelmartinsmachado25@gmail.com>
2024-12-22 23:08:01 -0300

    9-aula-exercicio

--- curso-react-labenu/src/App.js
+++ curso-react-labenu/src/App.js
@@ -1,21 +1,66 @@
-import React, { useState } from "react"
-import GlobalStyle from "./GlobalStyle"
-import { Garagem } from "./Componentes/Garagem/Garagem"
+import React, {useState} from 'react'
+import styled, {createGlobalStyle} from 'styled-components'
+import PokemonCard from './Components/PokemonCard/PokemonCard';
 
+const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`
+const FlexContainer = styled.div`
+  display: flex;
+  align-items: center;
+`
+function App() {
+  const [pokemon, setPokemon] = useState({
+    name: "Pichu",
+    type: "Electric",
+    evolved: false,
+    weight: 2,
+    color: 'yellow',
+    image: 'https://archives.bulbagarden.net/media/upload/thumb/b/b9/172Pichu.png/250px-172Pichu.png',
+    id: 0
+  })
 
-export default function App(){
+  const [pokemon2, setPokemon2] = useState({
+    name: "Pichu",
+    type: "Electric",
+    evolved: false,
+    weight: 2,
+    color: 'yellow',
+    image: 'https://archives.bulbagarden.net/media/upload/thumb/b/b9/172Pichu.png/250px-172Pichu.png',
+    id: 0
+  })
 
-  const [nome, setNome] = useState("Labenu")
-
-  return(
-
-
-    <div className="App">
-      <GlobalStyle/>
-
-      <Garagem nome={nome} alterarNome={setNome} />
-   
-    </div>
-  )
-}
   
+
+
+
+ 
+  // Para fazer seus próximos pokemons, crie novos estados!
+
+  return ( <>
+  <GlobalStyles/>
+    <FlexContainer>
+      {     
+      /* Aqui neste componente, passe as props. Lembre-se que também é possivel passar a função de setState via props! */
+      }
+      <PokemonCard 
+       pokemon={pokemon}
+       setPokemon={setPokemon}
+      
+       
+      
+     /* Crie aqui seus próximos pokemons! */
+    />
+
+   
+    </FlexContainer>
+  </>
+    
+  );
+}
+
+export default App;
